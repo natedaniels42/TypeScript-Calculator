2be2a74c6597d02b7222510d105a7522542ae23e
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-11-29 13:32:18 -0600

    Finished styling

--- calc.js
+++ calc.js
@@ -1,32 +1,72 @@
 var output = document.querySelector('.output');
+var numStr = '';
+var numStrArr = [''];
+var operand;
+var calculated = false;
 /*declare the following variables and types
-  num1 as number
-  num2 as number
-  operand as string
-  
-  Figure out how to set the values of num1 and num2 as the buttons are clicked
-  */
-document.querySelector('.calc').addEventListener('click', function (evt) {
-    var button = evt.target;
-    if (button.className.indexOf('num') !== -1) {
-        console.log(button.innerHTML);
-        output.value += button.innerHTML;
-    }
-    if (button.className.indexOf('operator') !== -1) {
-        console.log(button.innerHTML);
-        output.value += button.innerHTML;
-    }
-    if (button.className.indexOf('equal') !== -1) {
-        console.log(button.innerHTML);
-        var calculation = calculate( /*pass in params here*/);
-        output.value = calculation.toString();
-    }
-    /* create another condition to clear the value of the input when C is pressed*/
-});
-//create a function that takes in 2 numbers and a string (operand)
-function calculate( /*You fill in here*/) {
-    /* based on the operand, find the correct value of the 2 numbers.  i.e., 4+5=9, 4*5=20, etc
-    return that value
-    */
-    return 0;
+num1 as number
+num2 as number
+operand as string
+
+Figure out how to set the values of num1 and num2 as the buttons are clicked
+*/
+var calc = document.getElementById('calc');
+if (calc) {
+    calc.addEventListener('click', function (evt) {
+        var button = evt.target;
+        if (calculated) {
+            output.value = '';
+            calculated = false;
+        }
+        if (button.className.indexOf('num') !== -1) {
+            console.log(button.innerHTML);
+            numStr += button.innerHTML;
+            output.value += button.innerHTML;
+        }
+        if (button.className.indexOf('operator') !== -1 && numStr.length !== 0) {
+            console.log(button.innerHTML);
+            console.log(numStr);
+            numStrArr[0] = numStr;
+            numStr = '';
+            operand = button.innerHTML;
+            output.value += button.innerHTML;
+        }
+        if (button.className.indexOf('equal') !== -1
+            && numStr.length !== 0
+            && numStrArr[0].length !== 0
+            && operand.length !== 0) {
+            console.log(button.innerHTML);
+            var calculation = calculate(numStrArr[0], numStr, operand);
+            output.value = calculation.toString();
+        }
+        if (button.className.indexOf('reset') !== -1) {
+            output.value = '';
+            numStr = '';
+            operand = '';
+            numStrArr[0] = '';
+        }
+        /* create another condition to clear the value of the input when C is pressed*/
+    });
 }
+//create a function that takes in 2 numbers and a string (operand)
+var calculate = function (strNum1, strNum2, operator) {
+    var num1 = Number(strNum1);
+    var num2 = Number(strNum2);
+    var result;
+    if (operator === '+') {
+        result = num1 + num2;
+    }
+    else if (operator === '-') {
+        result = num1 - num2;
+    }
+    else if (operator === '*') {
+        result = num1 * num2;
+    }
+    else {
+        result = num1 / num2;
+    }
+    numStr = '';
+    numStrArr[0] = '';
+    calculated = true;
+    return result;
+};
--- calc.ts
+++ calc.ts
@@ -1,35 +1,75 @@
 let output = document.querySelector('.output') as HTMLInputElement;
-  /*declare the following variables and types
+
+let numStr: string = ''; 
+
+let numStrArr: string[] = [''];
+let operand: string;
+let calculated: boolean = false;
+    /*declare the following variables and types
     num1 as number
     num2 as number
     operand as string
     
     Figure out how to set the values of num1 and num2 as the buttons are clicked
     */
-document.querySelector('.calc').addEventListener('click',(evt)=>{
-    let button = evt.target as HTMLButtonElement;
-  
-    if(button.className.indexOf('num')!==-1){
-        console.log(button.innerHTML);      
-        output.value += button.innerHTML;
-    }
-    if(button.className.indexOf('operator')!==-1){
-        console.log(button.innerHTML);      
-        output.value += button.innerHTML;
-    }
-    if(button.className.indexOf('equal')!==-1){
-        console.log(button.innerHTML);
-        let calculation = calculate(/*pass in params here*/);
-        output.value = calculation.toString();
-    }
-    /* create another condition to clear the value of the input when C is pressed*/
-});
+const calc: HTMLElement | null = document.getElementById('calc');
+if (calc) {
+    calc.addEventListener('click',(evt)=>{
+        let button = evt.target as HTMLButtonElement;
+        
+        if (calculated) {
+            output.value = '';
+            calculated = false;
+        }
+        if(button.className.indexOf('num')!==-1){
+            console.log(button.innerHTML); 
+            numStr += button.innerHTML;     
+            output.value += button.innerHTML;
+        }
+        if(button.className.indexOf('operator')!==-1 && numStr.length !== 0){
+            console.log(button.innerHTML); 
+            console.log(numStr); 
+            numStrArr[0] = numStr;
+            numStr = '';
+            operand = button.innerHTML;    
+            output.value += button.innerHTML;
+        }
+        if(button.className.indexOf('equal')!==-1 
+            && numStr.length !== 0 
+            && numStrArr[0].length !== 0 
+            && operand.length !== 0){
+            console.log(button.innerHTML);
+            let calculation = calculate(numStrArr[0], numStr, operand);
+            output.value = calculation.toString();
+        }
+        if (button.className.indexOf('reset') !== -1) {
+            output.value = '';
+            numStr = '';
+            operand = '';
+            numStrArr[0] = '';
+        }
+        /* create another condition to clear the value of the input when C is pressed*/
+    });
+}
 
 //create a function that takes in 2 numbers and a string (operand)
-function calculate(/*You fill in here*/){
-    /* based on the operand, find the correct value of the 2 numbers.  i.e., 4+5=9, 4*5=20, etc
-    return that value
-    */
-   return 0;
+const calculate = (strNum1: string, strNum2: string, operator: string): number => {
+    const num1: number = Number(strNum1);
+    const num2: number = Number(strNum2);
+    let result: number;
 
+    if (operator === '+') {
+        result = num1 + num2;
+    } else if (operator === '-') {
+        result = num1 - num2;
+    } else  if (operator === '*') {
+        result = num1 * num2;
+    } else {
+        result = num1 / num2;
+    }
+
+    numStr = '';
+    numStrArr[0] = '';
+    calculated = true;
+    return result;
 }
